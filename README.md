7b933a32c44a80d27362630f696bd704b501935e
Matteo Tortora <matteo.tortora@gmail.com>
2022-05-30 01:18:38 +0200

    upd

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">
-<img src="logo/pytracklogo.svg" width="300">
+<img src="https://raw.githubusercontent.com/cosbidev/PyTrack/main/logo/pytracklogo.svg" width="300">
 </h1><br>
 
 -----------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='PyTrack-lib',
-    version='1.0.0',
+    version='1.0.1',
     package_dir={"": "pytrack"},
     packages=setuptools.find_packages(where="pytrack"),
     url='https://github.com/cosbidev/PyTrack',
