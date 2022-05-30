958afb9a68e4bf46e0162646c2638af985168a42
Matteo Tortora <matteo.tortora@gmail.com>
2022-05-30 12:16:36 +0200

    upd

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 import setuptools
-import os
-
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = lib_folder + '/requirements.txt'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open(requirement_path) as f:
+with open("requirements.txt") as f:
     INSTALL_REQUIRES = [line.strip() for line in f.readlines()]
 
 setuptools.setup(
     name='PyTrack-lib',
-    version='1.0.1',
-    package_dir={"": "pytrack"},
-    packages=setuptools.find_packages(where="pytrack"),
+    version='1.0.7',
+    packages=setuptools.find_packages(),
+    namespace_packages=['pytrack'],
     url='https://github.com/cosbidev/PyTrack',
     license='BSD-3-Clause-Clear',
     author='Matteo Tortora',
@@ -27,5 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    install_reqs=INSTALL_REQUIRES
+    install_requires=INSTALL_REQUIRES
 )
