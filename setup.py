632bdef824300e9d1713669950e3b460ebc241ac
Matteo Tortora <matteo.tortora@gmail.com>
2022-07-06 20:14:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='PyTrack-lib',
-    version='1.0.3',
+    version='2.0.0',
     packages=setuptools.find_packages(),
     # namespace_packages=['pytrack'],
     url='https://github.com/cosbidev/PyTrack',
